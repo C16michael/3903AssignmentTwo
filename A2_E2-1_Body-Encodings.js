ca32b4fb10ce01e249ee03b97092ee7237894795
Cristiano Michael <c16michael@gmail.com>
2023-10-18 08:59:55 -0500

    Exercise 2.1 Complete

--- A2_E2-1_Body-Encodings.js
+++ A2_E2-1_Body-Encodings.js
@@ -18,7 +18,30 @@ app.get('/', (req, res) =>{
     res.sendFile(path.join(__dirname + "/homepage.html"));
 })
 
-
+app.post('/raw-text', (req, res) => {
+    res.send(`myValue: ${req.body.rawTextValue}`);
+  });
+  
+  // Handle POST requests for JSON-encoded data
+  app.post('/jsonEncoded', (req, res) => {
+    res.json({ myValue: req.body.jsonValue });
+  });
+  
+  // Handle POST requests for URL-encoded data
+  app.post('/urlEncoded', (req, res) => {
+    res.send(`myValue: ${req.body.urlEncodedValue}`);
+  });
+  
+  // Handle POST requests for Multipart data
+  app.post('/multipart', (req, res) => {
+    formParser.parse(req, (err, fields) => {
+      if (!err && fields.multipartValue && fields.multipartValue[0]) {
+        res.send(`myValue: ${fields.multipartValue[0]}`);
+      } else {
+        res.status(500).send('Error parsing multipart data');
+      }
+    });
+  });
 
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`);
